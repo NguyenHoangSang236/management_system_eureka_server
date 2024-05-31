85c227d48b83b4c0c0790f9ebfc0ad5f33d6d17f
“NguyenHoangSang236” <nguyenhoangsang236@gmail.com>
2024-06-01 01:25:19 +0700

    init project

---
+++ src/test/java/com/management_system/eureka_server/EurukaServerApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.management_system.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurukaServerApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
