0aae1076f4218156c05415cdb36e14a1b14acd77
“NguyenHoangSang236” <nguyenhoangsang236@gmail.com>
2024-07-11 00:11:32 +0700

    deploy

--- build_eureka_server.sh
+++ build_eureka_server.sh
@@ -52,5 +52,5 @@ fi
 # Xóa container cũ, build và run container mới
 docker rm -f eureka-server-container
 docker build -t eureka-server-image .
-docker run -p8761:8761 --name eureka-server-container eureka-server-image
+docker run -d -p 8761:8761 --name eureka-server-container --network root_my_network eureka-server-image
 EOF
