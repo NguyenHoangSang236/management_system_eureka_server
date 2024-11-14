1b31e85c71fce1462bee2f5e904811a6b54c55bd
“NguyenHoangSang236” <nguyenhoangsang236@gmail.com>
2024-11-14 23:55:30 +0700

    update config

--- src/main/java/com/management_system/eureka_server/EurekaServerApplication.java
+++ src/main/java/com/management_system/eureka_server/EurekaServerApplication.java
@@ -1,14 +1,25 @@
 package com.management_system.eureka_server;
 
+import com.management_system.utilities.utils.InitiateConfigUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
+	@Autowired
+    InitiateConfigUtils initiateConfigUtils;
+
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 
+	@EventListener(ApplicationReadyEvent.class)
+	public void initSslConfig() {
+		initiateConfigUtils.initSslConfig();
+	}
 }
